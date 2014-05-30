a46b75215e1a4d9394a440a88f890d1fb7370561
Ryan Koven <celeritas@ucla.edu>
2014-05-30 15:52:05 -0400

    Add TwistAndFoldSort

--- mayhem.js
+++ mayhem.js
@@ -34,6 +34,40 @@ Mayhem.prototype.casinoSort = function(arr){
 		}
 	}
 	return arr;
-}
+};
+
+Mayhem.prototype.twist = function(arr){
+	var i = 0;
+	var temp1 = arr[i];
+	for (; i < arr.length; i++){
+		temp2 = arr[(i + 1)%arr.length];
+		arr[(i + 1)%arr.length] = temp1;
+		temp1 = temp2; 
+	}
+};
+
+Mayhem.prototype.nTwist = function(arr, n){
+	for (var i = 0; i < n; i++){
+		this.twist(arr);
+	}
+};
+
+Mayhem.prototype.foldUpTo = function(arr, upTo){
+	for (var i = 0, j = upTo; i < j; i++, j--){
+		temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+};
+
+/* Bogo-type sorts */
+
+Mayhem.prototype.twistAndFoldSort = function(arr){
+	while(!this.isSorted(arr)){
+		this.nTwist(arr, Math.floor(Math.random()*arr.length));
+		this.foldUpTo(arr, Math.floor(Math.random()*arr.length));
+	}
+	return arr;
+};
 
 module.exports = Mayhem;
--- test/mayhemTest.js
+++ test/mayhemTest.js
@@ -1,5 +1,6 @@
 var Mayhem = require('../mayhem'),
 		mayhem = new Mayhem(function(arr){}),
-		list = [3, 2, 1, 14, 14, 15, 17, 19, 18];
+		list = [3, 2, 1, 14, 14, 15, 17, 19, 18, 5, 29];
 
+console.log(mayhem.twistAndFoldSort(list));
 console.log(mayhem.casinoSort(list));
